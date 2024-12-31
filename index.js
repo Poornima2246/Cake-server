38ba804f7c9e109fe259c787663b306640460dd0
poornima2246 <poornima220500@gmail.com>
2024-12-31 21:28:27 +0530

    commit

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.use(express.static(path.join(__dirname, "frontend/build"))); // Serve static
 
 // ======== Fallback for SPA ========
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/build"));
+  res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
 });
 
 // ======== Start Server ========
