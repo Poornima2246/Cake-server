5f5d5ff9a71fe91352ca1e7f520cef55bbd04b32
poornima2246 <poornima220500@gmail.com>
2024-12-31 22:43:17 +0530

    commit

--- index.js
+++ index.js
@@ -1,34 +1,4 @@
-// import express from "express";
-// import cors from "cors";
-// import dotenv from "dotenv";
-// import { connectDB } from "./config/db.js";
-// import CakeRoute from "./routes/CakeRoute.js";
-
-// dotenv.config(); // Load environment variables
-
-// const app = express();
-// const port = process.env.PORT || 4000;
-
-// // Middleware
-// app.use('/upload', express.static('upload')); // Serve uploaded images
-// app.use(express.json());
-// app.use(cors({ origin: '*' })); // Allow requests from frontend
-
-// // Connect to MongoDB 
-// connectDB();
-
-// // API routes
-// app.use("/api/dessert", CakeRoute);
-
-// // Default route
-// app.get('/', (req, res) => {
-//     res.send('Server is working');
-// });
-
-// // Start server
-// app.listen(port, () => {
-//     console.log(`Server running at http://localhost:${port}`);
-// });
+ 
 
 
 import express from "express";
@@ -56,11 +26,11 @@ app.use("/api/dessert", CakeRoute); // Route for dessert-related APIs
 
 // ======== Serve Static Frontend ========
 const __dirname = path.resolve(); // Resolve the current directory
-app.use(express.static(path.join(__dirname, "frontend/build"))); // Serve static React files
+app.use(express.static(path.join(__dirname, "../frontend/build"))); // Serve static React files
 
 // ======== Fallback for SPA ========
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/build"));
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
 
 // ======== Start Server ========
